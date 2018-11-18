e189dc6c516e2ad668c1ea878312da82767243ba
MARCIN GARSTKA <marcin.garstka@windowslive.com>
2018-11-18 21:32:20 +0100

    mg

--- playbooks/roles/_IMAGE_set_disk_persistent/templates/setPersistentMode.ps1
+++ playbooks/roles/_IMAGE_set_disk_persistent/templates/setPersistentMode.ps1
@@ -12,10 +12,11 @@ Param(
 )
 
 #Import PowerCLI
-. "C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1" 
+#. "C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
+. "C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
 
 #Connect to vCenter
-Connect-VIServer -Server vcs01.lab.local -User Creator@vsphere.local -Password xxxx
+Connect-VIServer -Server vcs01.lab.local -User administrator@vsphere.local -Password P@ssw0rd
 
 #Set Persistent Mode
 get-harddisk -VM $CompHost | where {$_.Name -eq "$vdiskname2"} | Set-Harddisk -Persistence "IndependentPersistent" -Confirm:$False
